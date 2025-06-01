39053685a26e3ce90533446b32ceb2dd83c8e056
Harshit <140914170+HARSHIT3210@users.noreply.github.com>
2025-06-01 14:34:12 +0530

    Project Setup

---
+++ src/app/companions/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Companion = () => {
+  return (
+    <div>Companion</div>
+  )
+}
---
+++ src/app/subscriptions/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Subscription = () => {
+  return (
+    <div>Subscription
+    
+    </div>
+  )
+}
