a7d81f894142eb8c8df0415bb01ef27bb8727ddd
Harshit <140914170+HARSHIT3210@users.noreply.github.com>
2025-06-19 12:32:04 +0530

    final chnages

--- src/components/CompanionCard.tsx
+++ src/components/CompanionCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,43 +7,36 @@ interface CompanionCardProps {
   name: string;
   topic: string;
   subject: string;
-  duration: string;
+  duration: number;
   color: string;
 }
 
-export default function CompanionCard({
+const CompanionCard = ({
   id,
   name,
   topic,
   subject,
   duration,
   color,
-}: CompanionCardProps) {
+}: CompanionCardProps) => {
   return (
     <article className="companion-card" style={{ backgroundColor: color }}>
-      <div className="flex justify-between items-center p-4">
+      <div className="flex justify-between items-center">
         <div className="subject-badge">{subject}</div>
-        <button className="companion-bookmark">
-          <Image
-            src="/icons/bookmark.svg"
-            alt="Companion Icon"
-            width={20}
-            height={20}
-          />
-        </button>
       </div>
+
       <h2 className="text-2xl font-bold">{name}</h2>
       <p className="text-sm">{topic}</p>
       <div className="flex items-center gap-2">
         <Image
-          className="bg-white rounded-full"
-          src={"/icons/clock.svg"}
+          src="/icons/clock.svg"
           alt="duration"
-          width={14.5}
-          height={14.5}
+          width={13.5}
+          height={13.5}
         />
-        <p className="text-sm"> {duration} minutes</p>
+        <p className="text-sm">{duration} minutes</p>
       </div>
+
       <Link href={`/companions/${id}`} className="w-full">
         <button className="btn-primary w-full justify-center">
           Launch Lesson
@@ -51,4 +44,6 @@ export default function CompanionCard({
       </Link>
     </article>
   );
-}
+};
+
+export default CompanionCard;
